1f935d022234ddb424b55bc145e0fc77e1d4bca1
Ross-Gosling <rossjgosling@gmail.com>
2016-06-01 14:07:14 +0100

    Generalised capitalise() Method

--- src/uk/co/cherrygoose/cleanerchat/systems/Utils.java
+++ src/uk/co/cherrygoose/cleanerchat/systems/Utils.java
@@ -4,33 +4,33 @@ public class Utils
 {
 	public static String capitalise(String string)
 	{
-		// Declares string for colour name
-		String colourName = "";
+		// Declares string for output
+		String output = "";
 		
-		// For all characters in the colour name
+		// For all characters in the string
 		for (int i = 0; i < string.length(); i++)
 		{
-			// If the char is first in the name
+			// If the char is first in the string
 			if(i == 0)
 			{
 				// Char toUpperCase
-				colourName += Character.toUpperCase(string.charAt(i));
+				output += Character.toUpperCase(string.charAt(i));
 			}
 			// If char is after an underscore
 			else if(string.charAt(i - 1) == '_') 
 			{
 				// Char toUpperCase
-				colourName += Character.toUpperCase(string.charAt(i));
+				output += Character.toUpperCase(string.charAt(i));
 			}
 			// Else
 			else 
 			{
 				// Char toLowerCase
-				colourName += Character.toLowerCase(string.charAt(i));
+				output += Character.toLowerCase(string.charAt(i));
 			}
 		}
 		
 		// Returns resultant string
-		return colourName;
+		return output;
 	}
 }
